ff81bc51a3aa0269209ff3f486fb56b9344343c3
Tian <tianbian1024@hotmail.com>
2018-07-28 16:27:54 +0800

    changed tfidf

--- TimeSeries.py
+++ TimeSeries.py
@@ -3,6 +3,9 @@ import json
 import os
 import math
 import jieba
+from sklearn import feature_extraction
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.feature_extraction.text import CountVectorizer
 
 def resolveJson(path):
     file = open(path, "rb")
@@ -193,16 +196,33 @@ def get_corpus(filedir):
     #print("corpus_list:" + str(corpus_list))
     return corpus_list
 
+def get_tfidf1(corpus):
+    corpus = ','.join(jieba.cut(corpus))
+    corpus = corpus.split(",")
+    print(corpus)
+    vectorizer = CountVectorizer()  # 该类会将文本中的词语转换为词频矩阵，矩阵元素a[i][j] 表示j词在i类文本下的词频
+    transformer = TfidfTransformer()  # 该类会统计每个词语的tf-idf权值
+    tfidf = transformer.fit_transform(
+        vectorizer.fit_transform(corpus))  # 第一个fit_transform是计算tf-idf，第二个fit_transform是将文本转为词频矩阵
+    word = vectorizer.get_feature_names()  # 获取词袋模型中的所有词语
+    weight = tfidf.toarray()  # 将tf-idf矩阵抽取出来，元素a[i][j]表示j词在i类文本中的tf-idf权重
+    for i in range(len(weight)):  # 打印每类文本的tf-idf词语权重，第一个for遍历所有文本，第二个for便利某一类文本下的词语权重
+        print
+        u"-------这里输出第", i, u"类文本的词语tf-idf权重------"
+        for j in range(len(word)):
+            print
+            word[j], weight[i][j]
+    return weight
 
 def NNinput(result,filedir,corpus_list):
     words=output(result) #按间隔分微博
     num_corpus = len(corpus_list)
     with open('F:\论文汇总\谣言检测\数据\A16rumdect\words.txt', 'w+') as cur_file:
-        for i in range(num_corpus):
-            word_tfidf = get_tfidf(corpus_list[i], corpus_list)
-            print("word_tfidf:"+str(word_tfidf))
-            for cur_word in word_tfidf:
-                cur_file.write(cur_word + ":" + str(word_tfidf[cur_word]) + "\n")
+        #for i in range(num_corpus):
+        word_tfidf = get_tfidf1(corpus_list)
+        print("word_tfidf:"+str(word_tfidf))
+        #for cur_word in word_tfidf:
+        #    cur_file.write(cur_word + ":" + str(word_tfidf[cur_word]) + "\n")
                 # cur_file.close()
                 #print("cur_word:"+cur_word + ":" + str(word_tfidf[cur_word]))
     return True
