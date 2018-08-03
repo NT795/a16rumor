a4c12670c2d0607a9a755d50bb25ebc75e5aa283
Tian <tianbian1024@hotmail.com>
2018-08-03 13:34:45 +0800

    get max 5000 tfidf for each interval

--- TimeSeries.py
+++ TimeSeries.py
@@ -3,6 +3,7 @@ import json
 import os
 import math
 import jieba
+import heapq
 from sklearn import feature_extraction
 from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.feature_extraction.text import CountVectorizer
@@ -66,17 +67,19 @@ def find_lcsubstr(s1, s2):
     for i in range(len(s1)):
         for j in range(len(s2)):
             if s1[i]==s2[j]:
-             m[i+1][j+1]=m[i][j]+1
+                m[i+1][j+1]=m[i][j]+1
             if m[i+1][j+1]>mmax:
                 mmax=m[i+1][j+1]
-            p=i+1
-    return (p-mmax,p,mmax) #s1[p-mmax:p]是最长子串
+                p=i
+    return (p-mmax+1,p,mmax) #s1[p-mmax:p]是最长子串
 
 def output(result):
+    inputN = 50
     L=max(result[1])-min(result[1])
     l=L/inputN
     k=0
-    lcsk=[0]*100
+    #lcsk=[0]*100
+    count=[0]*100
     while 1:
         k=k+1
         N=L/l
@@ -84,87 +87,41 @@ def output(result):
         s1=[0]*N
         s2=[1]*N
         interval=[0]*(N+1)
-        for step in range(N):
+        for step in range(N+1):
             interval[step]=min(result[1])+step*l
         for step in range(N):
             i=0
             for i in range(result[0]):
                 if interval[step]<result[1][i]<interval[step+1]:
                     s1[step]=1
-        lcs=find_lcsubstr(s1,s2)
-        lcsk[k]=lcs[2]
-        if lcsk[k]<inputN and lcsk[k]>lcsk[k-1]:
+        for i in range(N):
+            if s1[i]==1:
+                count[k] = count[k] + 1
+        #lcs=find_lcsubstr(s1,s2)
+        #lcsk[k]=lcs[2]
+        #if lcsk[k]<inputN and lcsk[k]>lcsk[k-1]:
+        if count[k] < inputN and count[k] > count[k - 1]:
             l=l/2
         else:
-            print("间隔为%d-%d,共%d个间隔，每个间隔%d个时间单位"%(interval[lcs[0]],interval[lcs[1]],N,l))
+            #print("间隔为%d-%d,共%d个间隔，每个间隔%d个时间单位"%(interval[lcs[0]],interval[lcs[1]],N,l))
+            print("间隔为%d-%d,共%d个间隔，每个间隔%d个时间单位,有微博的间隔有%d个" % (interval[0], interval[N], N, l,count[k]))
             n=0
-            words=[None]*N
-            for x in range(N):
-                for y in range(result[0]):
-                    if interval[lcs[0]]+x*l < result[1][y] <interval[lcs[0]]+(x+1)*l:
-                        words[x]=words[x].append(result[2][y])
+            #words=['']*lcsk[k]
+            #for x in range(lcsk[k]):
+            words = [''] * count[k]
+            x=0
+            for y in range(N):
+                if s1[y] == 1:
+                    words[x] = []
+                    for z in range(result[0]):
+                        #print(interval[lcs[0]]+x*l ,result[1][y] ,interval[lcs[0]]+(x+1)*l)
+                        #if interval[lcs[0]]+x*l < result[1][y] <interval[lcs[0]]+(x+1)*l:
+                        if interval[0] + y * l <= result[1][z] <= interval[0] + (y + 1) * l:
+                            #words[x]=words[x].append(result[2][y])
+                            words[x].append(z)
+                            #print(result[2][z])
+                    x=x+1
             return (words)
-
-
-def get_tf(text):
-    """
-    计算tf值
-    text:该词所在文档，此处需要对text文本进行分词
-    return:dict word_tf(该文本出现的词的tf值)
-    """
-    #print("text1"+str(text))
-    text=','.join(jieba.cut(text))
-    text=text.split(",")
-    print("text"+str(text))
-    num_words = len(text)
-    word_freq = {}  # 词频dict
-    word_tf = {}  # 词的tf值dict
-    for i in range(num_words):
-        word_count = 1
-        for j in range(num_words):
-            if i != j and text[i] != " ":
-                if text[i] == text[j]:
-                    word_count += 1
-                    #text[j] = " "
-        if text[i] != " ":
-            # word_freq[text[i]] = word_count
-            word_tf[text[i]] = float(word_count / num_words)
-    return word_tf
-
-
-def get_idf(word, corpus_list):
-    """
-    计算idf值
-    word：要计算的词
-    corpus_list:包含所有语料的list，一个文件为其中一个元素
-    return:该词的idf值
-    """
-    num_corpus = len(corpus_list)
-    count = 0
-    for cur_corpus in corpus_list:
-        cur_corpus = ','.join(jieba.cut(cur_corpus))
-        cur_corpus = cur_corpus.split(",")
-        if word in cur_corpus:
-            print(str(word)+str(count))
-            count += 1
-    idf = math.log(float(num_corpus / (count + 1)))
-    return idf
-
-
-def get_tfidf(cur_corpus, corpus_list):
-    """
-    分文本计算tfidf值
-    cur_corpus:当前文本
-    corpus_list：所有文本的list
-    """
-    cur_word_tfidf = {}
-    word_tf = get_tf(cur_corpus)
-    print("word_tf:"+str(word_tf))
-    for word in word_tf:
-        tf = word_tf[word]
-        idf = get_idf(word, corpus_list)
-        cur_word_tfidf[word] = tf * idf
-    return cur_word_tfidf
 
 
 def get_corpus(filedir):
@@ -196,51 +153,88 @@ def get_corpus(filedir):
     #print("corpus_list:" + str(corpus_list))
     return corpus_list
 
-def get_tfidf1(corpus):
-    corpus = ','.join(jieba.cut(corpus))
-    corpus = corpus.split(",")
-    print(corpus)
+
+def get_least_numbers_big_data(alist, k):
+    max_heap = []
+    length = len(alist)
+    if k <= 0 or k > length:
+        alist.extend([0] * (k - len(alist)))
+        return alist
+    k = k - 1
+    for ele in alist:
+        #ele = -ele 求最小数时取反
+        if len(max_heap) <= k:
+            heapq.heappush(max_heap, ele)
+        else:
+            heapq.heappushpop(max_heap, ele)
+    return max_heap
+
+
+def get_tfidf(result,real_corpus):
+    """
+    real_corpus=[]
+    index=0
+    with open('F:\论文汇总\谣言检测\数据\A16rumdect\\real_corpus.txt', mode='w', encoding='utf-8') as fw:
+        for cur_corpus in corpus:
+            index=index+1
+            print("%d/3093836"%index)
+            cur_corpus = ' '.join(jieba.cut(cur_corpus))
+            real_corpus.append(cur_corpus)
+            fw.write(str(cur_corpus)+ '\n')
+            #print(real_corpus)
+    """
     vectorizer = CountVectorizer()  # 该类会将文本中的词语转换为词频矩阵，矩阵元素a[i][j] 表示j词在i类文本下的词频
     transformer = TfidfTransformer()  # 该类会统计每个词语的tf-idf权值
     tfidf = transformer.fit_transform(
-        vectorizer.fit_transform(corpus))  # 第一个fit_transform是计算tf-idf，第二个fit_transform是将文本转为词频矩阵
-    word = vectorizer.get_feature_names()  # 获取词袋模型中的所有词语
-    weight = tfidf.toarray()  # 将tf-idf矩阵抽取出来，元素a[i][j]表示j词在i类文本中的tf-idf权重
+        vectorizer.fit_transform(real_corpus))  # 第一个fit_transform是计算tf-idf，第二个fit_transform是将文本转为词频矩阵
+    #word = vectorizer.get_feature_names()  # 获取词袋模型中的所有词语
+    weiboInIntervals=output(result) #按间隔分微博
+    num_interval = len(weiboInIntervals)
+    max_tfidf=[]
+    for i in range(num_interval):
+        cur_tfidf=tfidf[weiboInIntervals[i]]
+        #for cur_weibo in weibono:
+        #cur_tfidf.append(tfidf[cur_weibo])
+        weight = cur_tfidf.data.tolist() # 将tf-idf矩阵中数据抽取出来
+        #print("max_k:")
+        # 取前5000大的值存入文件
+        max_k = get_least_numbers_big_data(weight, 5000)
+        #print(max_k)
+        max_tfidf.append(max_k)
+    #print("max_tfidf:")
+    #print(max_tfidf)
+    """
     for i in range(len(weight)):  # 打印每类文本的tf-idf词语权重，第一个for遍历所有文本，第二个for便利某一类文本下的词语权重
         print
         u"-------这里输出第", i, u"类文本的词语tf-idf权重------"
         for j in range(len(word)):
             print
             word[j], weight[i][j]
-    return weight
+    """
+    return max_tfidf
 
-def NNinput(result,filedir,corpus_list):
-    words=output(result) #按间隔分微博
-    num_corpus = len(corpus_list)
-    with open('F:\论文汇总\谣言检测\数据\A16rumdect\words.txt', 'w+') as cur_file:
-        #for i in range(num_corpus):
-        word_tfidf = get_tfidf1(corpus_list)
-        print("word_tfidf:"+str(word_tfidf))
-        #for cur_word in word_tfidf:
-        #    cur_file.write(cur_word + ":" + str(word_tfidf[cur_word]) + "\n")
-                # cur_file.close()
-                #print("cur_word:"+cur_word + ":" + str(word_tfidf[cur_word]))
+
+def NNinput(result,filename,corpus_list):
+    with open('F:\论文汇总\谣言检测\数据\A16rumdect\dataset\\'+str(filename)+'words.txt', 'w+') as cur_file:
+        max_tfidf = get_tfidf(result,corpus_list)
+        for i in range(len(max_tfidf)):
+            cur_file.write(max_tfidf[i])
+            cur_file.write("\n")
     return True
 
 
 filedir='F:\论文汇总\谣言检测\数据\A16rumdect\Weibo'
 #corpus_list = get_corpus(filedir)
 corpus_list = []
-f = open(str("F:\论文汇总\谣言检测\数据\A16rumdect\corpus.txt"),encoding='utf-8')
+f = open(str("F:\论文汇总\谣言检测\数据\A16rumdect\\real_corpus.txt"),encoding='utf-8')
 #cur_file = f.read()
-cur_file = f.read().replace("\n"," ")
-cur_file = cur_file.split(" ")
+cur_file = f.read().replace("\n","\\")
+cur_file = cur_file.split("\\")
 corpus_list.extend(cur_file)
 f.close()
 #print("corpus_list:"+str(corpus_list))
-inputN=10
 for filename in os.listdir(filedir):
     path = filedir + '/' + filename
     result = resolveJson(path)
-    NNinput(result,filedir,corpus_list)
-    #读取每个文件前5000大的tfidf值[,,,]，换行写入一个文件中
+    NNinput(result,filename,corpus_list)
+    #计算每个事件里每个时间间隔前5000大的tfidf值，换行写入一个文件中
