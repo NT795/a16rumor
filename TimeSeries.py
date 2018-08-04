6b40f99ff16688767b32e4e145790cfaf2570494
Tian <tianbian1024@hotmail.com>
2018-08-04 18:58:09 +0800

    a faster way

--- TimeSeries.py
+++ TimeSeries.py
@@ -11,7 +11,12 @@ from sklearn.feature_extraction.text import CountVectorizer
 def resolveJson(path):
     file = open(path, "rb")
     fileJson = json.load(file)
-    length=len(fileJson)
+    ori_length=len(fileJson)
+    length=ori_length
+    for index in range(ori_length):
+        if fileJson[index]["text"]=='':
+            length=length-1
+            #print("空")
     reposts_count=[None]*length
     uid=[None]*length
     bi_followers_count=[None]*length
@@ -34,32 +39,40 @@ def resolveJson(path):
     gender=[None]*length
     comments_count=[None]*length
     t=[None]*length
-    for index in range(length):
-        reposts_count[index]=fileJson[index]["reposts_count"]
-        uid[index] = fileJson[index]["uid"]
-        bi_followers_count[index] = fileJson[index]["bi_followers_count"]
-        text[index] = fileJson[index]["text"]
-        user_description[index] = fileJson[index]["user_description"]
-        user_avatar[index] = fileJson[index]["user_avatar"]
-        id[index] = fileJson[index]["id"]
-        city[index] = fileJson[index]["city"]
-        friends_count[index] = fileJson[index]["friends_count"]
-        province[index] = fileJson[index]["province"]
-        user_location[index] = fileJson[index]["user_location"]
-        followers_count[index] = fileJson[index]["followers_count"]
-        verified_type[index] = fileJson[index]["verified_type"]
-        picture[index] = fileJson[index]["picture"]
-        statuses_count[index] = fileJson[index]["statuses_count"]
-        parent[index] = fileJson[index]["parent"]
-        verified[index] = fileJson[index]["verified"]
-        favourites_count[index] = fileJson[index]["favourites_count"]
-        username[index] = fileJson[index]["username"]
-        gender[index] = fileJson[index]["gender"]
-        comments_count[index] = fileJson[index]["comments_count"]
-        t[index] = fileJson[index]["t"]
-    return (length,t,text,reposts_count,uid,bi_followers_count,user_description,user_avatar,id,city,province,friends_count,user_location,followers_count,verified_type,statuses_count,picture,parent,verified,favourites_count,username,gender,comments_count)
+    count=0
+    for index in range(ori_length):
+        if fileJson[index]["text"]=='':
+            count=count+1
+            continue
+        else:
+            t[index-count] = fileJson[index]["t"]
+            text[index-count] = fileJson[index]["text"]
+            """
+             reposts_count[index-count]=fileJson[index]["reposts_count"]
+             uid[index-count] = fileJson[index]["uid"]
+             bi_followers_count[index-count] = fileJson[index]["bi_followers_count"]
+             user_description[index-count] = fileJson[index]["user_description"]
+             user_avatar[index-count] = fileJson[index]["user_avatar"]
+             id[index-count] = fileJson[index]["id"]
+             city[index-count] = fileJson[index]["city"]
+             friends_count[index-count] = fileJson[index]["friends_count"]
+             province[index-count] = fileJson[index]["province"]
+             user_location[index-count] = fileJson[index]["user_location"]
+             followers_count[index-count] = fileJson[index]["followers_count"]
+             verified_type[index-count] = fileJson[index]["verified_type"]
+             picture[index-count] = fileJson[index]["picture"]
+             statuses_count[index-count] = fileJson[index]["statuses_count"]
+             parent[index-count] = fileJson[index]["parent"]
+             verified[index-count] = fileJson[index]["verified"]
+             favourites_count[index-count] = fileJson[index]["favourites_count"]
+             username[index-count] = fileJson[index]["username"]
+             gender[index-count] = fileJson[index]["gender"]
+             comments_count[index-count] = fileJson[index]["comments_count"]
+             """
+    #return (length,t,text,reposts_count,uid,bi_followers_count,user_description,user_avatar,id,city,province,friends_count,user_location,followers_count,verified_type,statuses_count,picture,parent,verified,favourites_count,username,gender,comments_count)
+    return (length,t,text)
 
-
+"""
 def find_lcsubstr(s1, s2):
     m=[[0 for i in range(len(s2)+1)]  for j in range(len(s1)+1)]  #生成0矩阵，为方便后续计算，比字符串长度多了一列
     mmax=0   #最长匹配的长度
@@ -72,6 +85,7 @@ def find_lcsubstr(s1, s2):
                 mmax=m[i+1][j+1]
                 p=i
     return (p-mmax+1,p,mmax) #s1[p-mmax:p]是最长子串
+"""
 
 def output(result):
     inputN = 50
@@ -103,8 +117,7 @@ def output(result):
         if count[k] < inputN and count[k] > count[k - 1]:
             l=l/2
         else:
-            #print("间隔为%d-%d,共%d个间隔，每个间隔%d个时间单位"%(interval[lcs[0]],interval[lcs[1]],N,l))
-            print("间隔为%d-%d,共%d个间隔，每个间隔%d个时间单位,有微博的间隔有%d个" % (interval[0], interval[N], N, l,count[k]))
+            #print("间隔为%d-%d,共%d个间隔，每个间隔%d个时间单位,有微博的间隔有%d个" % (interval[0], interval[N], N, l,count[k]))
             n=0
             #words=['']*lcsk[k]
             #for x in range(lcsk[k]):
@@ -151,6 +164,18 @@ def get_corpus(filedir):
                     fw.write(str(weibo).replace(' ','')+'\n')
                     #fw.write("\n".encode('utf-8'))
     #print("corpus_list:" + str(corpus_list))
+    """
+     corpus_list=[]
+     index=0
+     with open('F:\论文汇总\谣言检测\数据\A16rumdect\\real_corpus.txt', mode='w', encoding='utf-8') as fw:
+         for cur_corpus in corpus_list:
+             index=index+1
+             print("%d/3093836"%index)
+             cur_corpus = ' '.join(jieba.cut(cur_corpus))
+             real_corpus.append(cur_corpus)
+             fw.write(str(cur_corpus)+ '\n')
+             #print(corpus_list)
+     """
     return corpus_list
 
 
@@ -170,71 +195,54 @@ def get_least_numbers_big_data(alist, k):
     return max_heap
 
 
-def get_tfidf(result,real_corpus):
-    """
-    real_corpus=[]
-    index=0
-    with open('F:\论文汇总\谣言检测\数据\A16rumdect\\real_corpus.txt', mode='w', encoding='utf-8') as fw:
-        for cur_corpus in corpus:
-            index=index+1
-            print("%d/3093836"%index)
-            cur_corpus = ' '.join(jieba.cut(cur_corpus))
-            real_corpus.append(cur_corpus)
-            fw.write(str(cur_corpus)+ '\n')
-            #print(real_corpus)
-    """
-    vectorizer = CountVectorizer()  # 该类会将文本中的词语转换为词频矩阵，矩阵元素a[i][j] 表示j词在i类文本下的词频
-    transformer = TfidfTransformer()  # 该类会统计每个词语的tf-idf权值
-    tfidf = transformer.fit_transform(
-        vectorizer.fit_transform(real_corpus))  # 第一个fit_transform是计算tf-idf，第二个fit_transform是将文本转为词频矩阵
-    #word = vectorizer.get_feature_names()  # 获取词袋模型中的所有词语
+def get_tfidf(sum,result,tfidf):
     weiboInIntervals=output(result) #按间隔分微博
     num_interval = len(weiboInIntervals)
     max_tfidf=[]
     for i in range(num_interval):
-        cur_tfidf=tfidf[weiboInIntervals[i]]
-        #for cur_weibo in weibono:
-        #cur_tfidf.append(tfidf[cur_weibo])
+        cur_tfidf=tfidf[list(map(lambda x:x+sum,weiboInIntervals[i]))]
         weight = cur_tfidf.data.tolist() # 将tf-idf矩阵中数据抽取出来
-        #print("max_k:")
         # 取前5000大的值存入文件
         max_k = get_least_numbers_big_data(weight, 5000)
-        #print(max_k)
         max_tfidf.append(max_k)
-    #print("max_tfidf:")
-    #print(max_tfidf)
-    """
-    for i in range(len(weight)):  # 打印每类文本的tf-idf词语权重，第一个for遍历所有文本，第二个for便利某一类文本下的词语权重
-        print
-        u"-------这里输出第", i, u"类文本的词语tf-idf权重------"
-        for j in range(len(word)):
-            print
-            word[j], weight[i][j]
-    """
     return max_tfidf
 
 
-def NNinput(result,filename,corpus_list):
+def NNinput(sum,result,filename,tfidf):
     with open('F:\论文汇总\谣言检测\数据\A16rumdect\dataset\\'+str(filename)+'words.txt', 'w+') as cur_file:
-        max_tfidf = get_tfidf(result,corpus_list)
+        max_tfidf = get_tfidf(sum,result,tfidf)
         for i in range(len(max_tfidf)):
             cur_file.write(str(max_tfidf[i]))
             cur_file.write("\n")
-    return True
+    return
 
 
 filedir='F:\论文汇总\谣言检测\数据\A16rumdect\Weibo'
 #corpus_list = get_corpus(filedir)
 corpus_list = []
 f = open(str("F:\论文汇总\谣言检测\数据\A16rumdect\\real_corpus.txt"),encoding='utf-8')
-#cur_file = f.read()
-cur_file = f.read().replace("\n","\\")
-cur_file = cur_file.split("\\")
+cur_file = f.read().replace("\n","\\\\")
+cur_file = cur_file.split("\\\\")
 corpus_list.extend(cur_file)
 f.close()
-#print("corpus_list:"+str(corpus_list))
+#print("corpus_list:"+str(len(corpus_list)))
+#corpus_list长度为3093837
+vectorizer = CountVectorizer()  # 该类会将文本中的词语转换为词频矩阵，矩阵元素a[i][j] 表示j词在i类文本下的词频
+transformer = TfidfTransformer()  # 该类会统计每个词语的tf-idf权值
+tfidf = transformer.fit_transform(
+    vectorizer.fit_transform(corpus_list))  # 第一个fit_transform是计算tf-idf，第二个fit_transform是将文本转为词频矩阵
+# word = vectorizer.get_feature_names()  # 获取词袋模型中的所有词语
+print("tfidf:"+str(tfidf.shape[0]))
+#tfidf.shape[0]长度为3093837
+filecount=0
+sum=0
 for filename in os.listdir(filedir):
+    filecount=filecount+1
+    print("分析第%d个文件，共%d个"%(filecount,len(os.listdir(filedir))))
     path = filedir + '/' + filename
     result = resolveJson(path)
-    NNinput(result,filename,corpus_list)
+    #sum有3093835条微博
+    NNinput(sum,result,filename,tfidf)
+    sum=sum+result[0]
+    #print("sum:"+str(sum))
     #计算每个事件里每个时间间隔前5000大的tfidf值，换行写入一个文件中
